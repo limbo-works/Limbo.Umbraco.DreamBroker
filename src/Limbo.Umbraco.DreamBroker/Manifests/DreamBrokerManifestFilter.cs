ae46efbf24b42f61c16518bb192bfa8581d5be22
Anders Bjerner <abjerner@skybrud.dk>
2023-07-20 20:59:51 +0200

    Updated package manifest filter to set "PackageId" property in Umbraco 12

--- src/Limbo.Umbraco.DreamBroker/Manifests/DreamBrokerManifestFilter.cs
+++ src/Limbo.Umbraco.DreamBroker/Manifests/DreamBrokerManifestFilter.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Reflection;
 using Umbraco.Cms.Core.Manifest;
 
 namespace Limbo.Umbraco.DreamBroker.Manifests {
@@ -8,7 +9,9 @@ namespace Limbo.Umbraco.DreamBroker.Manifests {
 
         /// <inheritdoc />
         public void Filter(List<PackageManifest> manifests) {
-            manifests.Add(new PackageManifest {
+
+            // Initialize a new manifest filter for this package
+            PackageManifest manifest = new() {
                 AllowPackageTelemetry = true,
                 PackageName = DreamBrokerPackage.Name,
                 Version = DreamBrokerPackage.InformationalVersion,
@@ -21,7 +24,21 @@ namespace Limbo.Umbraco.DreamBroker.Manifests {
                 Stylesheets = new [] {
                     $"/App_Plugins/{DreamBrokerPackage.Alias}/Styles/Default.css"
                 }
-            });
+            };
+
+            // The "PackageId" property isn't available prior to Umbraco 12, and since the package is build against
+            // Umbraco 10, we need to use reflection for setting the property value for Umbraco 12+. Ideally this
+            // shouldn't fail, but we might at least add a try/catch to be sure
+            try {
+                PropertyInfo? property = manifest.GetType().GetProperty("PackageId");
+                property?.SetValue(manifest, DreamBrokerPackage.Alias);
+            } catch {
+                // We don't really care about the exception
+            }
+
+            // Append the manifest
+            manifests.Add(manifest);
+
         }
 
     }
